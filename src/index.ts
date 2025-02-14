506e755e2602c4fa5f61450765eeb8f7d3b2d0b8
Etette Etok <etettetok5@gmail.com>
2025-02-14 18:38:25 +0100

    updated port

--- src/index.ts
+++ src/index.ts
@@ -160,7 +160,7 @@ const startAgents = async () => {
 
   directClient.start(serverPort);
 
-  if (serverPort !== parseInt(settings.SERVER_PORT || "3000")) {
+  if (serverPort !== parseInt(settings.PORT || "3000")) {
     elizaLogger.log(`Server started on alternate port ${serverPort}`);
   }
 
